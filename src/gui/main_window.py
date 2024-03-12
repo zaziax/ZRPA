30187b060e7f8b7331193919d836a6f2773853b4
什么都没有mx <3062379017@qq.com>
2024-03-12 22:31:24 +0800

    ui文件初稿

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,5 +1,8 @@
 from qfluentwidgets import SplitFluentWindow
-
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QAction, QHBoxLayout, QLabel
+from qfluentwidgets import RoundMenu, setTheme, Theme, Action, MenuAnimationType, MenuItemDelegate, CheckableMenu, MenuIndicatorType
+from qfluentwidgets import FluentIcon as FIF
 
 class MainWindow(SplitFluentWindow):
     def __init__(self):
