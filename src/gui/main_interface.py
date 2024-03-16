29133d340ea2cdbbbcb690d63b89b4a742e26848
什么都没有mx <3062379017@qq.com>
2024-03-16 22:43:18 +0800

    随手提交

--- src/gui/main_interface.py
+++ src/gui/main_interface.py
@@ -1,8 +1,19 @@
-from PyQt5.QtWidgets import QWidget
-
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget,QAction
+#导入QIcon
+from PyQt5.QtGui import QIcon
 from src.gui.main_ui import Main_Ui
+from qfluentwidgets import Action
+from qfluentwidgets import FluentIcon as FIF
 
 class MainInterface(QWidget,Main_Ui):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.setupUi(self)
+        self.HeaderToolBar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        self.HeaderToolBar.addActions([
+            Action(FIF.ADD,self.tr('add')),
+            Action(FIF.DELETE,self.tr('delete'))
+        ])
+
+
